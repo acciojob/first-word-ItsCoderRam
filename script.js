c4b6db70e03cee7e2e90295ba6ccd11c4c7d8ed5
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-08-20 18:10:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstWord(s) {
   // your code here
+	if (s.length === 0) {
+        return '';
+    }
+
+    var firstSpaceIndex = s.indexOf(' '); 
+    if (firstSpaceIndex === -1) {
+        return s;
+    }
+
+    return s.slice(0, firstSpaceIndex);
 }
 
 // Do not change the code below
