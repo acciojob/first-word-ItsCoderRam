0777d47688a0840a18019132976d1b699504ba8b
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-09-01 15:18:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 function firstWord(s) {
   // your code here
-	if (s.length === 0) {
-        return '';
-    }
+	function firstWord(str) {
+    // split the string into an array of words
+    let words = str.split(' ');
 
-    var firstSpaceIndex = s.indexOf(' '); 
-    if (firstSpaceIndex === -1) {
-        return s;
-    }
-
-    return s.slice(0, firstSpaceIndex);
+    // return the first word
+    return words[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+ const s = prompt("Enter String:");
+ alert(firstWord(s));
